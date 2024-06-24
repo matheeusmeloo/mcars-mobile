39a5540f6fd207b55e3d192d23357a80107cb5c9
Matheus Melo <rmelomatheus@gmail.com>
2024-06-24 15:34:10 -0300

    Página de cadastro

--- cad.php
+++ cad.php
@@ -3,12 +3,16 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
+    <title>Cadastro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
     <link rel="icon" type="image/x-icon" href="./assets/icons/favicon-mcars.svg">
     <link rel="stylesheet" href="./css/style.css">
   </head>
   <body>
-        
+        <div class="container is-fluid">
+            <form action="" method="get">
+                
+            </form>
+        </div>
   </body>
 </html>
