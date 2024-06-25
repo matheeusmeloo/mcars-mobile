2f3f67b18edac0a25be8e10ccc2d4ea4a32ad7d4
Matheus Melo <rmelomatheus@gmail.com>
2024-06-25 15:32:29 -0300

    Add página cadastro simples

--- cad.php
+++ cad.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
     <link rel="icon" type="image/x-icon" href="./assets/icons/favicon-mcars.svg">
     <link rel="stylesheet" href="./css/style.css">
   </head>
---
+++ js/javascript.js
@@ -0,0 +1,94 @@
+
+// ================== AQUI COMEÇA AS INFORMAÇÕES DO MODAL ================
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+  
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+  
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+  
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+      const modal = $trigger.dataset.target;
+      const $target = document.getElementById(modal);
+  
+      $trigger.addEventListener('click', () => {
+        openModal($target);
+      });
+    });
+  
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+  
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+  
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      if(event.key === "Escape") {
+        closeAllModals();
+      }
+    });
+  });
+// =============== AQUI TERMINA O CÓDIGO DO MODAL ============
+
+
+// FUNÇÃO SIMPLES PARA MOSTRAR CARREGAMENTO FAKE AO CLICAR NOS BOTÕES
+function loading() {
+    var button = document.getElementById('btn-login');
+    button.classList.add('is-loading');
+    // Tempo para voltar ao normal
+    setTimeout(function() {
+        button.classList.remove('is-loading');
+    }, 3000);
+}
+function loading_fb() {
+  var button = document.getElementById('btn-login-facebook');
+  button.classList.add('is-loading');
+  // Tempo para voltar ao normal
+  setTimeout(function() {
+      button.classList.remove('is-loading');
+  }, 3000);
+}
+
+function loading_gg() {
+  var button = document.getElementById('btn-login-google');
+  button.classList.add('is-loading');
+  // Tempo para voltar ao normal
+  setTimeout(function() {
+      button.classList.remove('is-loading');
+  }, 3000);
+}
+
+function loading_apple() {
+  var button = document.getElementById('btn-login-apple');
+  button.classList.add('is-loading');
+  // Tempo para voltar ao normal
+  setTimeout(function() {
+      button.classList.remove('is-loading');
+  }, 3000);
+}
+
+// ===========================  FUNÇÃO PARA VOLTAR A PAGINA INICIAL E ABRIR O MODAL DE LOGIN
+
+document.getElementById('indexLink').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevenir o comportamento padrão do link
+  window.history.back(); // Voltar uma página no histórico
+
+  // Exibir o modal
+  document.getElementById('modal-js-example').style.display = 'block';
+});
+
+// ==================== FIM DA FUNÇÃO DE VOLTAR A PÁGINA INICIAL
